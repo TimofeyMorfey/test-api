635e0eeb169bb16a6f591a3b92ee5cefc181bc23
Morfey <Morfey@mail.com>
2025-11-27 20:41:27 +0300

    ApiService getSales

---
+++ app/Services/ApiService.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class ApiService
+{
+    /**
+     * Create a new class instance.
+     */
+    private string $baseUrl;
+
+    private string $apikey;
+
+    public function __construct()
+    {
+        $this->baseUrl = 'http://' . env('API_HOST', '109.73.206.144:6969');
+        $this->apikey = env('API_KEY', 'E6kUTYrYwZq2tN4QEtyzsbEBk3ie');
+    }
+
+    /**
+     * @param string $dateFrom
+     * @param string $dateTo
+     * @return array|null
+     */
+    public function getSales(string $dateFrom, string $dateTo): ?array
+    {
+        try {
+            $url = $this->baseUrl . '/api/sales';
+
+            $response = Http::timeout(30)
+            ->retry(3, 100)
+            ->get($url, [
+                'dateFrom' => $dateFrom,
+                'dateTo' => $dateTo,
+                'key' => $this->apikey,
+                'limit' => 100,
+                'page' => 1,
+
+            ]);
+
+            if ($response->successful()) {
+                return $response->json();
+            } else {
+                Log::error('API SALES Error: ' . $response->status(), [
+                    'status' => $response->status(),
+                    'body' => $response->body(),
+                ]);
+                return null;
+            }
+        } catch (\Exception $e) {
+            Log::error('API SALES Exeption: ' . $e->getMessage());
+        }
+    }
+
+}
